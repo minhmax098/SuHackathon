d32ab79c09e2ea3b627e7f4ee13a94ab4e3c5eea
Lê Học Minh <hocminh1998@gmail.com>
2026-01-28 14:43:08 +0300

    commit source code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,6 @@ export default function Home() {
         <Navbar />
         <MainImage />
 
-        // Scroll here
         <div id="about" style={{ padding: 24 }}>About section...</div>
         <div id="tutorials" style={{ padding: 24 }}>Tutorials section...</div>
         <div id="methods" style={{ padding: 24 }}>Methods section...</div>
