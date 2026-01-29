e40e0f658c27d3654aa7ddca4ed3b73eb758973f
Lê Học Minh <hocminh1998@gmail.com>
2026-01-29 21:19:00 +0300

    commit source code

--- client/src/components/MethodsModal.tsx
+++ client/src/components/MethodsModal.tsx
@@ -26,7 +26,7 @@ export default function MethodsModal({ open, onClose }: Props) {
 
             <h4 style={styles.h4}>AWC from Probe Readings</h4>
             <p style={styles.p}>
-            Example formulas:
+            Example formulas: 
             </p>
 
             <pre style={styles.code}>
